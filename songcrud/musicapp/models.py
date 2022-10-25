78174b2c4644dc2996630966d7b564bad971595d
Tonymaria <114440159+Tonymaria@users.noreply.github.com>
2022-10-25 21:58:00 +0100

    I have updated the models.py

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Artiste (models.Model):
+    first_name = models.CharField (max_length=100)
+    last_name = models.CharField (max_length=100) 
+    age = models.DateField() 
+
+    def __str__ (self):
+        return self.first_name
+
+class Song (models.Model):
+    artiste = models.ForeignKey (Artiste, on_delete = models.CASCADE)
+    title = models.CharField (max_length=100)
+    date_released = models.IntegerField()
+    likes = models.CharField (max_length=500)
+    artiste_id = models.IntegerField()
+
+
+    def __str__ (self):
+        return self.title
+
+class Lyric (models.Model):
+    song = models.ForeignKey (Song, on_delete = models.CASCADE)
+    content = models.CharField (max_length=500)
+    song_id = models.IntergerField ()
+
+    def __str__ (self):
+        return self.song
